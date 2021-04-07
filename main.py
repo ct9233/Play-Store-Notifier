3d37d4e521cb5491b1f7fb760cdaee943f8eeef8
Chris Twaddell <ct9233@gmail.com>
2021-04-06 17:57:55 -0700

    Eliminated Variable Hardcoding

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 from google_play_scraper import app
 from twilio.rest import Client
+from datetime import datetime, timedelta
 
+load_dotenv("C:/data/.env")
 account_sid = os.getenv("TWI_ACC_SID")
 auth_token = os.getenv("TWI_AUTH_TOK")
 twilio_from = os.getenv("TWI_FROM_NUM")
 twilio_to = os.getenv("TWI_TO_NUM")
 
-slay_spire_price = 9.99
-slay_spire_version = "2.2.6"
+yesterday_timestamp = datetime.timestamp(datetime.now() - timedelta(days=1))
 notification_message = ''
 
 result = app(
@@ -18,13 +19,11 @@ result = app(
     country='us'
 )
 
-if result['price'] != slay_spire_price:
-    notification_message = f"The Google Play Store price for Slay the Spire has changed to {result['price']}."
-elif result['free'] == True:
+if result['free'] == True:
     notification_message = "Slay the Spire is now free on the Google Play Store!"
 elif result['sale'] == True:
-    notification_message = "Slay the Spire is on sale on the Google Play Store for ${result['price']} during {result['saleTime']}."
-elif result['version'] != slay_spire_version:
+    notification_message = f"Slay the Spire is on sale on the Google Play Store for ${result['price']} during {result['saleTime']}."
+elif result['updated'] > yesterday_timestamp:
     notification_message = f"Slay the Spire version has changed to {result['version']} on the Google Play Store.\nChanges: {result['recentChanges']}"
     
 if notification_message != '':
